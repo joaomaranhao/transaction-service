ef175f43a178dc07b84f8ca01a7cd6bbb3d8aefe
João Maranhão <joaofelipemaranhao@gmail.com>
2026-02-22 16:48:43 -0300

    feat: create logger

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.api.routes import router
 from app.core.config import settings
+from app.core.logger import logger
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Iniciando o serviço de transações...")
+    yield
+    logger.info("Encerrando o serviço de transações...")
+
 
 app = FastAPI(
     title=settings.app_name,
     description="Serviço para gerenciamento de transações financeiras",
+    version="1.0.0",
+    lifespan=lifespan,
 )
 
 app.include_router(router)
